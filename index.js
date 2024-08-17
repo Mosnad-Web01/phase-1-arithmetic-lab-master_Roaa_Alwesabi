94e6588d84cc29957bd0907438832dbcda6d6f4e
Roaa2002-web <roaa.abdulfattah.alwesabi@gmail.com>
2024-08-17 16:26:02 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2; 
+
+let random = Math.floor(Math.random() * 100) + 1; 
+
+let num3 = 16;
+let num4 = 6;
+let mod = num3 % num4; 
+
+let max = Math.max(10, 15, 20, 5); 
+
+console.log(multiply, random, mod, max);
